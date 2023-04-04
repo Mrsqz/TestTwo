6f075ab5f0210e9b1ff23c3b8bd84223605f1484
qz <3066952123@qq.com>
2023-04-04 22:15:36 +0800

    这是一个index文件

---
+++ js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load' , function(){
+    //为音乐添加src 名称
+    document.querySelector('.audio').src = '../images/music.mp3';
+    document.querySelector('.musicname').innerHTML = '我不是梦';
+})
